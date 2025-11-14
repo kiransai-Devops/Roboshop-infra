0d4d7e2b0c8056aff2f1491b440f0e33a11b1014
kiransaiveerandi <kiransaiveerandi@gmail.com>
2025-11-14 22:38:52 +0530

    new

--- 00-vpc/perameters.tf
+++ 00-vpc/perameters.tf
@@ -7,5 +7,18 @@ resource "aws_ssm_parameter" "vpc_id" {
 resource "aws_ssm_parameter" "private_subnet_ids" {
   name  = "/${var.project_name}/${var.Env}/private_subnet_ids"
   type  = "StringList"
+  value = join("," , module.vpc.private_subnet_ids)
+}
+
+resource "aws_ssm_parameter" "public_subnet_ids" {
+  name  = "/${var.project_name}/${var.Env}/public_subnet_ids"
+  type  = "StringList"
   value = join("," , module.vpc.public_subnet_ids)
+}
+
+
+resource "aws_ssm_parameter" "database_subnet_ids" {
+  name  = "/${var.project_name}/${var.Env}/database_subnet_ids"
+  type  = "StringList"
+  value = join("," , module.vpc.database_subnet_ids)
 }
