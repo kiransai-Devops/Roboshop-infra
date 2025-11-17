d498a4c776b44368aedb8ae88006024ec8a48ec9
kiransaiveerandi <kiransaiveerandi@gmail.com>
2025-11-17 11:04:13 +0530

    new

--- 40-database/bootstrap.sh
+++ 40-database/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 component=$1
+environment=$2
 dnf install ansible -y
 #ansible-pull -U https://github.com/kiransai-Devops/ansible-roboshop-roles-tf.git -e component=$component main.yaml
 # git clone ansible-playbook
@@ -28,7 +29,7 @@ else
     cd $ANSIBLE_DIR
 fi
 
-ansible-playbook -e component=$component main.yaml
+ansible-playbook -e component=$component environment=$environment main.yaml
 
 
 
