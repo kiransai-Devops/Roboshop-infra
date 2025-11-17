02c6ab8f028e146b923aafc2addb0e1c469074bb
kiransaiveerandi <kiransaiveerandi@gmail.com>
2025-11-17 17:39:43 +0530

    new

--- 30-sg-rules/data.tf
+++ 30-sg-rules/data.tf
@@ -22,3 +22,7 @@ data "aws_ssm_parameter" "rabbitmq_sg_id" {
 data "aws_ssm_parameter" "mysql_sg_id" {
   name = "/${var.project_name}/${var.Env}/mysql_sg_id"
 }
+
+data "aws_ssm_parameter" "catalogue_sg_id" {
+  name = "/${var.project_name}/${var.Env}/catalogue_sg_id"
+}
